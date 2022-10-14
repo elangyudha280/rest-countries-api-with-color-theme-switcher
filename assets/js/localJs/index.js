e73f7b4669e29f3f240e9b3702358ba1ebc1b7e0
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-14 17:36:49 +0700

    selesai desain navbar dan fitur light and dark mode

---
+++ assets/js/localJs/index.js
@@ -0,0 +1,13 @@
+
+
+// fitur dark and light mode
+let btn_mode = document.querySelector('.btn-mode')
+let html_element = document.querySelector('html')
+btn_mode.addEventListener('click',function(){
+    if(html_element.dataset.colorMode === 'light'){
+        html_element.dataset.colorMode = 'dark';
+    }
+    else{
+        html_element.dataset.colorMode = 'light'
+    }
+})
