f945c7dc3e4592e0c214eb65fa3583dacb1fe010
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-17 12:48:52 +0700

    add modules parse number to string

---
+++ assets/js/modules/ParseToString.mjs
@@ -0,0 +1,8 @@
+
+
+// function to change number to format string
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export default numberWithCommas;
