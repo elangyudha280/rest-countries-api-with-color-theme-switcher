ca457a0558c3c7b743ffb6bf8585a86165ed6a11
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-18 18:32:12 +0700

    selesai konfigurasi

---
+++ assets/js/localJs/detail.js
@@ -0,0 +1,14 @@
+
+
+// fiturt light and dark mode
+
+let btn_mode = document.querySelector('.btn-mode')
+let html_element = document.querySelector('html')
+btn_mode.addEventListener('click',function(){
+    if(html_element.dataset.colorMode === 'light'){
+        html_element.dataset.colorMode = 'dark';
+    }
+    else{
+        html_element.dataset.colorMode = 'light'
+    }
+})
