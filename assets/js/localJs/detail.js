753266c52a33791bd16d6d7648217819b5d480b1
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-19 14:07:18 +0700

    selesai desain detail

--- assets/js/localJs/detail.js
+++ assets/js/localJs/detail.js
@@ -5,6 +5,7 @@
 let btn_mode = document.querySelector('.btn-mode')
 let html_element = document.querySelector('html')
 btn_mode.addEventListener('click',function(){
+    console.log('ok')
     if(html_element.dataset.colorMode === 'light'){
         html_element.dataset.colorMode = 'dark';
     }
